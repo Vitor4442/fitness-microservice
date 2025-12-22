ee6ce903791dabdf15a649bc1dc3969f24ee5e41
José Vitor <jose.vitorw018@gmail.com>
2025-12-22 20:49:28 -0300

    Criando o projeto

---
+++ userserci/src/main/java/com/fitness/userserci/UserserciApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.userserci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserserciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserserciApplication.class, args);
+	}
+
+}
---
+++ userserci/src/test/java/com/fitness/userserci/UserserciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.userserci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
